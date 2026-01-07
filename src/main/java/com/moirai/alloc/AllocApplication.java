7ea7216f864d496ebc30286fb2abb1f9412c8ffa
penrose0214 <penrose0214@gmail.com>
2026-01-07 09:30:55 +0900

    init: starting projects

---
+++ src/main/java/com/moirai/alloc/AllocApplication.java
@@ -0,0 +1,13 @@
+package com.moirai.alloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moirai/alloc/AllocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moirai.alloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
