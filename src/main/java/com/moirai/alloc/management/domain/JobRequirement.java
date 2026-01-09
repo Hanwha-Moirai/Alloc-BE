eb49df92d1a5caa3ea93f042141d4626e83ced5c
longlive13 <papiairplane@gmail.com>
2026-01-10 01:12:16 +0900

    refactor : JobRequirement 생성자 검증 및 접근 제어 추가

--- src/main/java/com/moirai/alloc/management/domain/JobRequirement.java
+++ src/main/java/com/moirai/alloc/management/domain/JobRequirement.java
@@ -2,6 +2,8 @@ package com.moirai.alloc.management.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 public class JobRequirement {
@@ -12,5 +14,21 @@ public class JobRequirement {
     private int requiredCount;
 
     protected JobRequirement() {}
+
+    protected JobRequirement(Long jobId, int requiredCount) {
+        if (jobId == null) {
+            throw new IllegalArgumentException("jobId는 필수");
+        }
+        if (requiredCount <= 0) {
+            throw new IllegalArgumentException("인원 수는 1 이상");
+        }
+        this.jobId = jobId;
+        this.requiredCount = requiredCount;
+    }
+
+    public Long getJobId() { return jobId; }
+    public int getRequiredCount() { return requiredCount; }
+
+    // 중복 값 생성 막는 생성자(equals, hashCode)에 대하여 추가할지 미정
 
 }
