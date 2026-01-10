289242df0a5ae1bb60d0edf2d0f6454852aa2e7a
longlive13 <papiairplane@gmail.com>
2026-01-10 20:26:52 +0900

    feat: add controller skeletons for presentation layer

---
+++ src/main/java/com/moirai/alloc/management/api/AssignmentController.java
@@ -0,0 +1,9 @@
+package com.moirai.alloc.management.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/assignments")
+public class AssignmentController {
+}
