fcfcaad9cd1691b85ba5b50dbd1cde0fa57cc2f1
longlive13 <papiairplane@gmail.com>
2026-01-10 19:11:43 +0900

    feat: define command/query service structure for project assignment domain

---
+++ src/main/java/com/moirai/alloc/management/query/GetAssignmentCandidates.java
@@ -0,0 +1,8 @@
+package com.moirai.alloc.management.query;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly=true)
+public class GetAssignmentCandidates {
+}
