c9a9b41745315d2de0e6aab6578cf733a4acedbb
longlive13 <papiairplane@gmail.com>
2026-01-12 15:06:32 +0900

    feat: add project register query DTO

--- src/main/java/com/moirai/alloc/management/query/dto/ProjectRegistrationViewDTO.java
+++ src/main/java/com/moirai/alloc/management/query/dto/ProjectRegistrationViewDTO.java
@@ -1,5 +1,9 @@
 package com.moirai.alloc.management.query.dto;
 
+import com.moirai.alloc.management.domain.entity.TechReqLevel;
+import com.moirai.alloc.project.command.domain.Project;
+import java.util.List;
+
 public class ProjectRegistrationViewDTO {
     private List<JobOptionDTO> jobOptions;
     private List<TechOptionDTO> techOptions;
