b6f2dd11ce0bdc23c16bad71246f32dfd9a409e6
longlive13 <papiairplane@gmail.com>
2026-01-12 14:14:23 +0900

    feat : add repository

--- src/main/java/com/moirai/alloc/management/query/GetProjectList.java
+++ src/main/java/com/moirai/alloc/management/query/GetProjectList.java
@@ -1,11 +1,15 @@
 package com.moirai.alloc.management.query;
+import com.moirai.alloc.management.domain.repo.ProjectRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly=true)
 public class GetProjectList {
-    //private final ProjectRepository projectRepository;
+    private final ProjectRepository projectRepository;
+    public GetProjectList(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
     public void getProjectList(Long userId){
 //        1) userId를 기준으로 사용자 식별
 //        2) 해당 사용자가 참여하고 참여했던(진행/종료) 프로젝트 목록을 조회한다.
