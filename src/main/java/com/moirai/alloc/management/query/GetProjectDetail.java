9d6168df701b064e522e65935dcbf81176d042b5
longlive13 <papiairplane@gmail.com>
2026-01-12 15:34:13 +0900

    feat: add project detail view query DTO, and query service

--- src/main/java/com/moirai/alloc/management/query/GetProjectDetail.java
+++ src/main/java/com/moirai/alloc/management/query/GetProjectDetail.java
@@ -1,14 +1,34 @@
 package com.moirai.alloc.management.query;
+import com.moirai.alloc.management.domain.repo.ProjectRepository;
+import com.moirai.alloc.management.query.dto.ProjectDetailViewDTO;
+import com.moirai.alloc.project.command.domain.Project;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly=true)
 public class GetProjectDetail {
-    //private final ProjectRepository projectRepository;
-    public void getProjectDetail(Long projectId) {
+    private ProjectRepository projectRepository;
+    public GetProjectDetail(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public ProjectDetailViewDTO getProjectDetail(Long projectId) {
 //        1) projectId로 프로젝트를 식별한다
 //        2) 프로젝트 상세 정보를 조회한다.
 //        3) 조회 결과를 반환한다
+        Project project = projectRepository.findById(projectId).orElseThrow(()->new IllegalArgumentException("Project not found!"));
+        return new ProjectDetailViewDTO(
+                project.getProjectId(),
+                project.getName(),
+                project.getProjectStatus(),
+                project.getProjectType(),
+                project.getStartDate(),
+                project.getEndDate(),
+                project.getPartners(),
+                project.getDescription(),
+                project.getPredictedCost()
+        );
     }
+
 }
---
+++ src/main/java/com/moirai/alloc/management/query/dto/ProjectDetailViewDTO.java
@@ -0,0 +1,27 @@
+package com.moirai.alloc.management.query.dto;
+
+import com.moirai.alloc.project.command.domain.Project;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class ProjectDetailViewDTO {
+    private Long projectId;
+    private String projectName;
+    private Project.ProjectStatus status;
+    private Project.ProjectType projectType;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String partners;
+    private String description;
+    private Integer predictedCost;
+
+    // 타 팀원
+    //progressRate;
+    //RISK
+    //ACTIVITY
+    // SCHEDULE
+}
