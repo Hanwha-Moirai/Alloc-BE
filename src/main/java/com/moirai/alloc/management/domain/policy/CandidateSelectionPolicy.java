80ca1aff9ef5a7a80cabccc04e49e616f2a2364f
longlive13 <papiairplane@gmail.com>
2026-01-12 12:24:41 +0900

    feat: add candidate selection policy contract and result model

--- src/main/java/com/moirai/alloc/management/domain/policy/CandidateSelectionPolicy.java
+++ src/main/java/com/moirai/alloc/management/domain/policy/CandidateSelectionPolicy.java
@@ -1,4 +1,7 @@
 package com.moirai.alloc.management.domain.policy;
 
+import com.moirai.alloc.project.command.domain.Project;
+
 public interface CandidateSelectionPolicy {
+    SelectionResult selectCandidates(Project project);
 }
--- src/main/java/com/moirai/alloc/management/domain/policy/SelectionResult.java
+++ src/main/java/com/moirai/alloc/management/domain/policy/SelectionResult.java
@@ -1,4 +1,15 @@
 package com.moirai.alloc.management.domain.policy;
 
+import java.util.List;
+
 public class SelectionResult {
+    private final List<Long> candidateUserIds;
+
+    public SelectionResult(List<Long> candidateUserIds) {
+        this.candidateUserIds = candidateUserIds;
+    }
+
+    public List<Long> getCandidateUserIds() {
+        return candidateUserIds;
+    }
 }
