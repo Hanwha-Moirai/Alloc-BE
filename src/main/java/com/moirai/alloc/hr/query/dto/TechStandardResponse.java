94bb0289cdce204d60de6e1b46ee344858eea063
dzmajin <qpalzm3415@gmail.com>
2026-01-12 19:09:54 +0900

    feat: 기술스택, 직군 드롭다운 조회 관련 로직 구현

---
+++ src/main/java/com/moirai/alloc/hr/query/dto/TechStandardResponse.java
@@ -0,0 +1,12 @@
+package com.moirai.alloc.hr.query.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TechStandardResponse {
+    private Long techId;
+    private String techName;
+}
+
