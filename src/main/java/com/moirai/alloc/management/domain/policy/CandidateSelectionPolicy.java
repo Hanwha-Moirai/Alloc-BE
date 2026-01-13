df8200f6e4d538e70e03321dc005acc9c78669f7
longlive13 <papiairplane@gmail.com>
2026-01-13 10:57:45 +0900

    chore: remove unused skill fit policy file

--- src/main/java/com/moirai/alloc/management/domain/policy/CandidateSelectionPolicy.java
+++
@@ -1,7 +0,0 @@
-package com.moirai.alloc.management.domain.policy;
-
-import com.moirai.alloc.project.command.domain.Project;
-
-public interface CandidateSelectionPolicy {
-    SelectionResult selectCandidates(Project project);
-}
