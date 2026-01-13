22adf8cb5159741b6e78803906bbffd5c029ecd0
penrose0214 <penrose0214@gmail.com>
2026-01-13 14:54:18 +0900

    fix: MyBatis 설정 제거

--- src/test/java/com/moirai/alloc/gantt/config/GanttMybatisTestConfig.java
+++
@@ -1,9 +0,0 @@
-package com.moirai.alloc.gantt.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration
-@MapperScan("com.moirai.alloc.gantt.query.mapper")
-public class GanttMybatisTestConfig {
-}
