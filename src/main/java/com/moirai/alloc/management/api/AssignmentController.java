36fe350931dadffc6667cf074245435da07eee31
longlive13 <papiairplane@gmail.com>
2026-01-13 15:53:25 +0900

    feat : add AssignmentController

--- src/main/java/com/moirai/alloc/management/api/AssignmentController.java
+++ src/main/java/com/moirai/alloc/management/api/AssignmentController.java
@@ -1,9 +1,25 @@
 package com.moirai.alloc.management.api;
 
+import com.moirai.alloc.management.command.dto.RegisterProjectCommandDTO;
+import com.moirai.alloc.management.command.service.RegisterProject;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/assignments")
+@RequiredArgsConstructor
 public class AssignmentController {
+    private final RegisterProject registerProject;
+
+    @PostMapping
+    public ResponseEntity<Long> registerProject(
+            @RequestBody RegisterProjectCommandDTO command
+    ) {
+        Long projectId = registerProject.registerProject(command);
+        return ResponseEntity.ok(projectId);
+    }
 }
