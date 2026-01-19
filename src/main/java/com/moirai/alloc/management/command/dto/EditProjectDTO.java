048fadea3012181fef87cd8fbdba46203d7fcc3d
longlive13 <papiairplane@gmail.com>
2026-01-19 09:35:31 +0900

    fix: add setters to EditProjectDTO to fix update not being applied

--- src/main/java/com/moirai/alloc/management/command/dto/EditProjectDTO.java
+++ src/main/java/com/moirai/alloc/management/command/dto/EditProjectDTO.java
@@ -1,10 +1,12 @@
 package com.moirai.alloc.management.command.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 @Getter
+@Setter
 public class EditProjectDTO {
     private Long projectId;
     private String projectName;
