30fb60a48aa522a6e66952619c76ee4ed6b8cdf8
longlive13 <papiairplane@gmail.com>
2026-01-19 12:46:25 +0900

    fix; delete annotation

--- src/main/java/com/moirai/alloc/management/api/ProjectController.java
+++ src/main/java/com/moirai/alloc/management/api/ProjectController.java
@@ -71,7 +71,7 @@ public class ProjectController {
     @PutMapping("/{projectId}")
     public void updateProject(
             @PathVariable Long projectId,
-            @Valid @RequestBody EditProjectDTO command
+            @RequestBody EditProjectDTO command
     ) {
         if (!projectId.equals(command.getProjectId())) {
             throw new IllegalArgumentException("Project ID mismatch");
