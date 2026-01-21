f88511c7ebfaf74afe6a071dd3ece181a006585b
longlive13 <papiairplane@gmail.com>
2026-01-21 17:20:01 +0900

    feat: implement GptSearchIntentParser with mock GPT response

--- src/main/java/com/moirai/alloc/search/query/infra/gpt/SearchIntentParser.java
+++ src/main/java/com/moirai/alloc/search/query/infra/gpt/SearchIntentParser.java
@@ -1,4 +1,7 @@
 package com.moirai.alloc.search.query.infra.gpt;
 
+import com.moirai.alloc.search.query.domain.model.SearchIntent;
+
 public interface SearchIntentParser {
+    SearchIntent parse(String naturalLanguage);
 }
