7523f35ed51e8386920fe2e1216a98f6124f3196
longlive13 <papiairplane@gmail.com>
2026-01-21 17:43:11 +0900

    fix: route people search API through use case layer

--- src/main/java/com/moirai/alloc/search/query/presentation/api/PeopleSearchController.java
+++ src/main/java/com/moirai/alloc/search/query/presentation/api/PeopleSearchController.java
@@ -1,23 +1,21 @@
 package com.moirai.alloc.search.query.presentation.api;
 
-import com.moirai.alloc.search.query.infra.openSearch.OpenSearchPersonSearcher;
 import com.moirai.alloc.search.query.infra.openSearch.PersonDocument;
 import com.moirai.alloc.search.query.presentation.dto.PersonView;
 import com.moirai.alloc.search.query.presentation.mapper.PersonViewMapper;
-import com.moirai.alloc.search.query.domain.model.SearchCondition;
+import com.moirai.alloc.search.query.service.SearchPeopleByNaturalLanguage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/people")
 public class PeopleSearchController {
-    private final OpenSearchPersonSearcher searchPeopleByNaturalLanguage;
+    private final SearchPeopleByNaturalLanguage searchPeopleByNaturalLanguage;
     private final PersonViewMapper mapper;
 
     @PostMapping("/search")
