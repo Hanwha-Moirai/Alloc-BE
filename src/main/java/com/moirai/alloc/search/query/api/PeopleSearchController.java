9cb6b7e91625be5ef68962df9b7a4adbe27ef0d2
longlive13 <papiairplane@gmail.com>
2026-01-21 16:20:56 +0900

    feat: add PeopleSearchController to expose person search API

--- src/main/java/com/moirai/alloc/search/query/api/PeopleSearchController.java
+++ src/main/java/com/moirai/alloc/search/query/api/PeopleSearchController.java
@@ -1,7 +1,40 @@
 package com.moirai.alloc.search.query.api;
 
+import com.moirai.alloc.search.query.infra.openSearch.OpenSearchPersonSearcher;
+import com.moirai.alloc.search.query.infra.openSearch.PersonDocument;
+import com.moirai.alloc.search.query.model.PersonView;
+import com.moirai.alloc.search.query.model.SearchCondition;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/people")
 public class PeopleSearchController {
+    private final OpenSearchPersonSearcher searcher;
+
+    @PostMapping("/search")
+    public List<PersonView> search(@RequestBody SearchCondition condition) {
+        List<PersonDocument> documents = searcher.search(condition);
+        return documents.stream()
+                .map(this::toView)
+                .toList();
+    }
+    private PersonView toView(PersonDocument doc) {
+        return PersonView.builder()
+                .personId(doc.getPersonId())
+                .name(doc.getName())
+                .jobTitle(doc.getJobTitle())
+                .techs(doc.getTechs())
+                .skillLevel(doc.getSkillLevel())
+                .activeProjectCount(doc.getActiveProjectCount())
+                .department(doc.getDepartment())
+                .workingType(doc.getWorkingType())
+                .build();
+    }
 }
